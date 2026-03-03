59538d871471629013e747295150273e65c7e4ab
khi195 <kashif.h.iqbal@gmail.com>
2026-03-03 22:54:36 +0000

    Add project files

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import { defineConfig } from 'vite'
+import tsConfigPaths from 'vite-tsconfig-paths'
+import viteReact from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import netlify from '@netlify/vite-plugin-tanstack-start'
+
+export default defineConfig({
+  server: {
+    port: 3000,
+  },
+  plugins: [
+    netlify(),
+    tailwindcss(),
+    tsConfigPaths({
+      projects: ['./tsconfig.json'],
+    }),
+    tanstackStart(),
+    viteReact(),
+  ],
+})
